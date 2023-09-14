aa2e05985008de48245209ad3135335c2602086a
Anssi Äijö <anssi.aijo@edu.karelia.fi>
2023-09-14 12:33:33 +0300

    Revert "Tehtava4"
    This reverts commit 98f4fb35cff5896358f402d857f67baacc4df6da.

--- App/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using AppLogger;
-
-const string message = "Hei_maailma";
-
-Console.WriteLine("perus: " + message);
-Logger.Log("Humanizer: " + message);
