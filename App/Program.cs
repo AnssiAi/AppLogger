98f4fb35cff5896358f402d857f67baacc4df6da
Anssi Äijö <anssi.aijo@edu.karelia.fi>
2023-09-14 12:29:35 +0300

    Tehtava4

---
+++ App/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AppLogger;
+
+const string message = "Hei_maailma";
+
+Console.WriteLine("perus: " + message);
+Logger.Log("Humanizer: " + message);
